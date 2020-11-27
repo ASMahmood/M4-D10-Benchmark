edecdebd35376df713fa9140cc5c5de90f65297e
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-27 09:18:50 +0000

    converted old navbar to react

--- react_spotify/src/App.js
+++ react_spotify/src/App.js
@@ -1,27 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
