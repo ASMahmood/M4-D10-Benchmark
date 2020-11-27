02bea387db853014d14c5e03dc3bba30abb2f3e0
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-27 10:06:12 +0000

    footer added and converted to react

--- react_spotify/src/App.js
+++ react_spotify/src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NavBar from "./components/NavBar";
+import FooterPlayer from "./components/FooterPlayer";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <NavBar />
+        <FooterPlayer />
       </BrowserRouter>
     );
   }
